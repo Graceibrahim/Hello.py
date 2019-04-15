74b248d8c27d724b79724f68c5b30e1e6ed429bc
Graceibrahim <47751148+Graceibrahim@users.noreply.github.com>
2019-04-15 17:33:50 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(c,d):
+	answer=c-d
+	return answer
+
+def divide(d,c):
+	answer=d/c
+	return answer
+
+def	modulus(x,y):
+	answer=x%y
+	return answer
+
+def multiplication(z,a):
+    answer=z*a
+    return answer
+
+def whole_number_division(f,d):
+	answer=f//d
+    return answer
+
+def exponent(a,q):
+    answer=a**q  
+    return answer
+
+def percentage(a,b):
+	answer=(a/b)*100
+    return answer
+	
+def percentage(b,c):
+	answer=(a/b)*100
+	return answer
